40f2f97ac62194f2fb250bb4178d507ff0db8cdc
Jason Washburn <jason.washburn@gmail.com>
2023-12-02 10:00:12 -0600

    Implement part one of day two

--- aoc/day_two/day2.py
+++ aoc/day_two/day2.py
@@ -3,6 +3,51 @@ import sys
 
 from aoc.common import parse_lines_from_file
 
+POSSIBLE_COLORS = {"red": 12, "green": 13, "blue": 14}
+
+
+def parse_grab(grab_str: str) -> dict[str, int]:
+    """Parses a grab string into a dict of block color counts.
+
+    Example: "3 blue, 4 red" -> {"red": 4, "green": 0, "blue": 3}
+
+    Args:
+        grab_str (str): a string representing color block counts
+
+    Returns:
+        dict[str, int]: a dict of color counts
+    """
+    result = {"red": 0, "green": 0, "blue": 0}
+    color_counts = grab_str.strip().split(",")
+    for color_count in color_counts:
+        count, color = color_count.strip().split(" ")
+        result[color] = int(count.strip())
+    return result
+
+
+def parse_game_line(input_line: str) -> list[dict[str, int]]:
+    result = []
+    _, grabs = input_line.strip().split(":")
+    for grab in grabs.strip().split(";"):
+        result.append(parse_grab(grab))
+    return result
+
+
+def sum_colors(color_counts: list[dict[str, int]]) -> dict[str, int]:
+    result = {"red": 0, "green": 0, "blue": 0}
+    for color_count in color_counts:
+        for color, count in color_count.items():
+            result[color] += count
+
+    return result
+
+
+def is_possible_grab(grab: dict[str, int], max_colors: dict[str, int]):
+    for color, count in grab.items():
+        if count > max_colors[color]:
+            return False
+    return True
+
 
 def part_one(input_lines: list[str]) -> int:
     """Solves part 1.
@@ -13,7 +58,19 @@ def part_one(input_lines: list[str]) -> int:
     Returns:
         int: The solution.
     """
-    ...
+    total = 0
+
+    for index, line in enumerate(input_lines):
+        game_num = index + 1
+        possible_game = True
+        game = parse_game_line(line)
+        for grab in game:
+            if not is_possible_grab(grab=grab, max_colors=POSSIBLE_COLORS):
+                possible_game = False
+                break
+        if possible_game:
+            total += game_num
+    return total
 
 
 def part_two(input_lines: list[str]) -> int:
--- tests/day_two/test_day2.py
+++ tests/day_two/test_day2.py
@@ -1,16 +1,68 @@
 """Tests for day 2."""
 import pytest
+from aoc.common import parse_lines_from_file
+from aoc.day_two.day2 import (
+    is_possible_grab,
+    parse_game_line,
+    parse_grab,
+    part_one,
+    part_two,
+    sum_colors,
+)
 
-from aoc.day_two.day2 import part_one, part_two
+
+def test_parse_game_line():
+    expected = [
+        {"red": 4, "green": 0, "blue": 3},
+        {"red": 1, "green": 2, "blue": 6},
+        {"red": 0, "green": 2, "blue": 0},
+    ]
+    assert (
+        parse_game_line("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+        == expected
+    )
 
 
 @pytest.mark.parametrize(
-    "input_lines, expected_output", [(["one", "two", "three"], 123)]
+    "grab, max_colors, expected",
+    [
+        ({"red": 1, "green": 2, "blue": 3}, {"red": 1, "green": 2, "blue": 3}, True),
+        ({"red": 2, "green": 2, "blue": 3}, {"red": 1, "green": 2, "blue": 3}, False),
+        ({"red": 1, "green": 3, "blue": 3}, {"red": 1, "green": 2, "blue": 3}, False),
+    ],
 )
-@pytest.mark.skip("Not Implemented")
-def test_part_one(input_lines, expected_output):
+def test_is_possible_grab(grab, max_colors, expected) -> None:
+    assert is_possible_grab(grab, max_colors) == expected
+
+
+@pytest.mark.parametrize(
+    "input_str, expected",
+    [
+        ("3 blue, 4 red", {"red": 4, "green": 0, "blue": 3}),
+        ("3 red, 4 blue", {"red": 3, "green": 0, "blue": 4}),
+        ("3 green, 4 red", {"red": 4, "green": 3, "blue": 0}),
+    ],
+)
+def test_parse_grab(input_str, expected) -> None:
+    """Tests parse grab."""
+    assert parse_grab(input_str) == expected
+
+
+def test_sum_colors():
+    """Tests sum_colors."""
+    assert sum_colors(
+        [
+            {"red": 4, "green": 3, "blue": 0},
+            {"red": 3, "green": 0, "blue": 4},
+            {"red": 4, "green": 0, "blue": 3},
+        ]
+    ) == {"red": 11, "green": 3, "blue": 7}
+
+
+def test_part_one():
     """Tests part one."""
-    assert part_one(input_lines) == expected_output
+    example = parse_lines_from_file("aoc/day_two/example1.txt")
+    assert part_one(example) == 8
 
 
 @pytest.mark.parametrize(
